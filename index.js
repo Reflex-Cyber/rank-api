70c5436b0cb3f4f2397b92eccb6a8ae8f968ffa7
Reflex-Cyber <skylenkorucu5@gmail.com>
2024-10-02 16:57:37 +0300

    Create index.js

---
+++ index.js
@@ -0,0 +1,34 @@
+require('dotenv').config();
+const express = require('express');
+const noblox = require('noblox.js');
+const app = express();
+
+app.use(express.json());
+
+app.use(async (req, res, next) => {
+    if (!cookie) {
+        return res.status(400).json({ error: "Cookie not set" });
+    }
+
+    await noblox.setCookie(cookie);
+    let currentUser = await noblox.getCurrentUser();
+    console.log(`Logged in as ${currentUser.UserName}`);
+    next();
+});
+
+app.post("/ranker", async (req, res) => {
+    try {
+        const { userid, rank, grupid, cookie } = req.body;
+        await noblox.setCookie(parseInt(cookie));
+        await noblox.setRank(parseInt(grupid), parseInt(userid), parseInt(rank));
+        res.json({ message: "Ranked!" });
+    } catch (error) {
+        console.error(error);
+        res.status(500).json({ error: "Internal server error" });
+    }
+});
+
+const PORT = process.env.PORT || 3000;
+app.listen(PORT, () => {
+    console.log(`Server is up and running on port ${PORT}`);
+});
